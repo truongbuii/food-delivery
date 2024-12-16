79d1f69925a8111613212f252e57980f18bda9de
truongbuii <truongbn.dev@gmail.com>
2024-12-16 22:50:18 +0700

    feat: add dark theme

--- client/src/app/(auth)/sign-up/page.tsx
+++ client/src/app/(auth)/sign-up/page.tsx
@@ -1,8 +1,11 @@
+import { ModeToggle } from '@/components/molecule/dark';
+
 const SignupPage = () => {
   return (
     <div className="absolute w-full px-6 pb-6">
       <div className="mt-24 w-full flex flex-col gap-7 mb-14">
         <span className="w-[247px] h-[40px]">Sign Up</span>
+        <ModeToggle />
       </div>
     </div>
   );
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from 'tailwindcss';
+import tailwindcssAnimate from 'tailwindcss-animate';
 
 export default {
+  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,12 +12,43 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)'
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))'
       },
       boxShadow: {
         socialBtn:
-          '18.58696px 18.58696px 37.17392px 0px rgba(211, 209, 216, 0.5)',
+          '18.58696px 18.58696px 37.17392px 0px rgba(211, 209, 216, 0.7)',
         primaryBtn: '0px 10px 20px 0px rgba(254, 114, 76, 0.50)'
       },
       animation: {
@@ -24,17 +57,38 @@ export default {
       },
       keyframes: {
         scaleUp: {
-          '0%, 100%': { transform: 'scale(1)' },
-          '50%': { transform: 'scale(1.25)' }
+          '0%, 100%': {
+            transform: 'scale(1)'
+          },
+          '50%': {
+            transform: 'scale(1.25)'
+          }
         },
         shiny: {
-          '0%': { transform: 'scale(0) rotate(45deg)', opacity: '0' },
-          '80%': { transform: 'scale(0) rotate(45deg)', opacity: '0.5' },
-          '81%': { transform: 'scale(4) rotate(45deg)', opacity: '1' },
-          '100%': { transform: 'scale(50) rotate(45deg)', opacity: '0' }
+          '0%': {
+            transform: 'scale(0) rotate(45deg)',
+            opacity: '0'
+          },
+          '80%': {
+            transform: 'scale(0) rotate(45deg)',
+            opacity: '0.5'
+          },
+          '81%': {
+            transform: 'scale(4) rotate(45deg)',
+            opacity: '1'
+          },
+          '100%': {
+            transform: 'scale(50) rotate(45deg)',
+            opacity: '0'
+          }
         }
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
       }
     }
   },
-  plugins: []
+  plugins: [tailwindcssAnimate]
 } satisfies Config;
