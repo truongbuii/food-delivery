9e6c94d47efb10ed72a22c6fb76efd545e8f2d94
truongbuii <truongbn.dev@gmail.com>
2024-12-16 21:01:20 +0700

    feat: fix git

---
+++ server/src/test/java/com/truongbuii/food_delivery/FoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truongbuii.food_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
