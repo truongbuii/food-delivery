00a2faa0edbd74c95296fb00a1c33349bfd846a2
truongbuii <truongbn.dev@gmail.com>
2025-03-14 19:30:24 +0700

    fix: wrapped in a suspense boundary

--- client/src/app/list/food/page.tsx
+++ client/src/app/list/food/page.tsx
@@ -1,7 +1,9 @@
 import FoodsByCategory from "@/components/features/food/foods-by-category";
 import { ButtonType } from "@/components/molecule";
+import { Skeleton } from "@/components/ui/skeleton";
 import { PATHNAME } from "@/configs";
 import BackgroundCategory from "@/layouts/BackgroundCategory";
+import { Suspense } from "react";
 
 const FoodCategoryPage = () => {
   return (
@@ -10,7 +12,18 @@ const FoodCategoryPage = () => {
       <BackgroundCategory>
         <div className="relative w-full px-6 pb-6 z-2">
           <div className="pt-28">
-            <FoodsByCategory />
+            <Suspense
+              fallback={
+                <div className="grid grid-cols-2 grid-rows-2 gap-2">
+                  <Skeleton className="h-10 w-10 rounded-md" />
+                  <Skeleton className="h-10 w-10 rounded-md" />
+                  <Skeleton className="h-10 w-10 rounded-md" />
+                  <Skeleton className="h-10 w-10 rounded-md" />
+                </div>
+              }
+            >
+              <FoodsByCategory />
+            </Suspense>
           </div>
         </div>
       </BackgroundCategory>
