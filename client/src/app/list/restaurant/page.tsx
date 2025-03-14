e8a8eb9524b8253f3f73689dd641c13862be6668
truongbuii <truongbn.dev@gmail.com>
2025-03-14 19:33:18 +0700

    fix: suspense for list restaurant

--- client/src/app/list/restaurant/page.tsx
+++ client/src/app/list/restaurant/page.tsx
@@ -1,6 +1,8 @@
 import RestaurantByParams from "@/components/features/restaurant/restaurants-by-params";
 import { ButtonType } from "@/components/molecule";
+import { Skeleton } from "@/components/ui/skeleton";
 import BackgroundCategory from "@/layouts/BackgroundCategory";
+import { Suspense } from "react";
 
 const ListRestaurantPage = () => {
   return (
@@ -9,7 +11,18 @@ const ListRestaurantPage = () => {
       <BackgroundCategory>
         <div className="relative w-full px-6 pb-6 z-2">
           <div className="pt-28">
-            <RestaurantByParams />
+            <Suspense
+              fallback={
+                <div className="grid grid-cols-2 grid-rows-2 gap-2">
+                  <Skeleton className="h-10 w-10 rounded-md" />
+                  <Skeleton className="h-10 w-10 rounded-md" />
+                  <Skeleton className="h-10 w-10 rounded-md" />
+                  <Skeleton className="h-10 w-10 rounded-md" />
+                </div>
+              }
+            >
+              <RestaurantByParams />
+            </Suspense>
           </div>
         </div>
       </BackgroundCategory>
